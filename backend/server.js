79176d76c2a9e1af34964f09e4fc19e2f95e8dba
Aniket Kumar <125212655+ErAniketKumar@users.noreply.github.com>
2024-11-12 09:43:03 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
 
-app.use(cors({ origin: "https://abhisheksarraf.netlify.app/", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 const session = require("express-session");
 const connectDB = require("./db/connection");
