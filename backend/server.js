4834acccc03fd8094b58924593aa009095419f3b
Aniket Kumar <125212655+ErAniketKumar@users.noreply.github.com>
2024-11-16 18:55:59 +0530

    server update

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,17 @@ const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
 
-app.use(cors());
+app.use(
+	cors({
+	  origin: 'https://abhisheksarraf.in', // Allow requests from this origin
+	  methods: 'GET, POST', // Specify allowed methods
+	  credentials: true,    // Allow cookies and other credentials
+	})
+  );
+
+// app.use(cors());
+
+
 
 const session = require("express-session");
 const connectDB = require("./db/connection");
@@ -30,6 +40,16 @@ app.use(
 connectDB();
 
 app.use("/api", router);
+
+
+app.options('/api/login', (req, res) => {
+	res.setHeader('Access-Control-Allow-Origin', 'https://abhisheksarraf.in');
+	res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	res.setHeader('Access-Control-Allow-Credentials', 'true');
+	res.status(200).end();
+  });
+  
 
 // for not found url / routes
 
